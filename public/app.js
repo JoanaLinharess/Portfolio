04b9b1574755128ccb6bb67e85f17bd508b17f05
Joana Linhares da Silva <joanaglinhares@gmail.com>
2021-05-07 01:22:22 -0300

    :beetle: aba contato quase funcionando

---
+++ public/app.js
@@ -0,0 +1,33 @@
+const contactForm = document.querySelector('.contact-form');
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let mensagem = document.getElementById("mensagem");
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let formData = {
+        nome: nome.value,
+        email: email.value,
+        subject: subject.value,
+        mensagem: mensagem.value
+    }
+    let xhr= new XMLHttpRequest();
+   
+    xhr.open('POST','/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if (xhr.responseText == 'success'){
+            alert ('Email enviado');
+            nome.value='';
+            email.value='';
+            subject.value='';
+            mensagem.value='';
+        }else{
+            alert('Algo errado, confira os dados.')
+        }
+    }
+    
+    xhr.send(JSON.stringify(formData));
+})
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require ('express')
 const app =express();
 
+const nodemailer = require("nodemailer");
+
 const PORT = process.env.PORT || 5000;
 
 
+app.use(express.urlencoded())
 app.use(express.static('public'));
 app.use(express.json())
 
@@ -13,8 +16,32 @@ app.get('/', (req, res)=>{
 
 app.post('/', (req, res)=>{
     console.log(req.body)
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user: 'contatojoanalinhares@gmail.com',
+            pass:'josilva1706'
+        }
+    
+    })
+    const mailOptions={
+        from:req.body.email,
+        to:'contatojoanalinhares@gmail.com',
+        subect:`Messsage from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+    transporter.sendMail(mailOptions,(error, info)=>{
+        if(erro){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email enviado:' + info.response);
+            res.send('success')
+        }
+    })
+
 })
 
 app.listen(PORT, () =>{
-    console.log('Server running on port ${PORT}')
+    console.log('Server running on port ${PORT}' + PORT)
 })
